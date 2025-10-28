d1662fd8c60d9a81e4d5399700e0fbd9555ce428
Jack Toumey <jack@toumey.io>
2025-10-27 18:52:03 -0700

    first function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn solve(a: f64, b: f64) -> f64 {
+  (a.powi(2) + b.powi(2)).sqrt()
+}
+
 fn main() {
-  println!("Hello, world!");
+  let a = 3.0;
+  let b = 4.0;
+
+  println!("Answer: {}", solve(3.0, 4.0));
+  println!("Answer: {}", solve(a, b));
 }
