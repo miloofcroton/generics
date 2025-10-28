3a98dd6538d63e45e99c871fac39985263ccd948
Jack Toumey <jack@toumey.io>
2025-10-27 19:32:26 -0700

    more use of traits

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use num_traits::ToPrimitive;
+use num_traits::{ToPrimitive, Float};
 
-fn solve(a: f64, b: f64) -> f64 {
-  (a.powi(2) + b.powi(2)).sqrt()
+fn solve<T: Float, U: Float>(a: T, b: U) -> f64 {
+  let a_f64 = a.to_f64().unwrap();
+  let b_f64 = b.to_f64().unwrap();
+
+  return (a_f64.powi(2) + b_f64.powi(2)).sqrt();
+}
+
+fn solve_v2<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
+  let a_f64 = a.to_f64().unwrap();
+  let b_f64 = b.to_f64().unwrap();
+
+  return (a_f64.powi(2) + b_f64.powi(2)).sqrt();
 }
 
 fn main() {
   let a: f32 = 3.0;
-  let b: f64 = 4.0;
+  let b: f32 = 4.0;
+  let c: i32 = 4;
 
-  let a_f64 = a.to_f64().unwrap();
+  // let a_f64 = a.to_f64().unwrap();
 
   println!("Answer: {}", solve(3.0, 4.0));
-  // println!("Answer: {}", solve(a, b));
-  println!("Answer: {}", solve(a_f64, b));
+  // println!("Answer: {}", solve(a_f64, b));
+  println!("Answer: {}", solve::<f32, f32>(a, b));
+  println!("Answer: {}", solve(a, b));
+  println!("Answer: {}", solve_v2(a, c));
 }
